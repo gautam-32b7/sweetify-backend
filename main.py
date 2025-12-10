c8a6fe008e635c6a9b97feb98e767f51a71f0def
gautam <gautam_32b7@hotmail.com>
2025-12-10 18:20:30 +0530

    add CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette import status
 
 from database import engine
@@ -7,6 +8,15 @@ from routers import desserts
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 dessert.Base.metadata.create_all(bind=engine)
